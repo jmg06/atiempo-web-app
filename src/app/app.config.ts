9a55bdfc80ca63f2cb3ca41cd5c4ad1cca7dc13f
jmg06 <jm.gutierrezg1@uniandes.edu.co>
2026-09-15 17:29:37 -0500

    Initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,8 @@
+import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+};
